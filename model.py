75f57b46ce27b2ebf419a2e0d7fbc7f71d937576
1758520lc <2658738922@qq.com>
2023-03-06 21:25:58 +0800

    recommend_project

---
+++ model.py
@@ -0,0 +1,13 @@
+#### 模型代码
+import torch
+import torch.nn as nn
+import dgl
+
+class GCN(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class GAT(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
